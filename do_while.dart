38f29c4b5fe1e44931841af7fb5102f9d13d5af1
Gustavo Tavares <gustavo.tavares24@outlook.com>
2023-07-21 14:42:01 -0300

    8: do while statement

---
+++ do_while.dart
@@ -0,0 +1,13 @@
+void main() {
+
+  int i = 10;
+  while(i > 10) { // nothing happens
+    print("Hello");
+  }
+
+  // Do While statement -> executes at least one time
+  do {
+    print("Hello"); // hello is printed
+  } while(i > 10);
+
+}
