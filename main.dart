d05fab87b7dbd98fd1b02b892669f366b0a8d1e2
Gustavo Tavares <gustavo.tavares24@outlook.com>
2023-07-21 12:01:48 -0300

    0: types and variables

--- main.dart
+++ main.dart
@@ -2,4 +2,21 @@
 
 void main() {
   print("Hello World!");
+
+  // Creating variables
+  // primitive types: int, String, double
+
+  int age = 21;
+  String name = "Gustavo";
+  double distance = 24.89;
+
+  print(age);
+  print(name);
+  print(distance);
+
+  age = 25;
+  name = "João";
+
+  print(age); // output: 25
+  print(name); // output: João
 }
