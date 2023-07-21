9c26369390a97a1a9d298cb04cfbc6eccfbb6a85
Gustavo Tavares <gustavo.tavares24@outlook.com>
2023-07-21 14:29:31 -0300

    5: ternary operator

---
+++ ternary.dart
@@ -0,0 +1,12 @@
+void main() {
+
+  //Ternary Operator: condition ? value1 : value2
+  double grade = 8.0;
+  String message = grade >= 5 ? "Approved" : "Reproved";
+
+  int number = grade == 5.0 && grade != 7 ? 10 : 2;
+
+  print(message);
+  print(number); 
+  
+}
