31bbcd6b46f83fac0efcbb78a8e776cd42457f35
Gustavo Tavares <gustavo.tavares24@outlook.com>
2023-07-21 14:17:58 -0300

    3: relatorional and logical operators

---
+++ logical_operators.dart
@@ -0,0 +1,23 @@
+void main() {
+
+  // Relational operators: ==, !=, >, <, >=, <=
+  int n = 10;
+  int n2 = 13;
+
+  // bool type -> true or false
+  bool r = n == 10;
+  print(r); // true
+
+  r = n != 10;
+  print(r); // false
+
+  r = n > n2;
+  print(r); // false
+
+  r = n < n2;
+  print(r); // true
+
+  // Logical operators: && || !
+  
+
+}
