0b317fbf57234e08e770f99bc7d4373239903421
gsTavares <gustavo.tavares24@outlook.com>
2023-10-25 20:12:26 -0300

    39: super keyword

--- poo/inheritance.dart
+++ poo/inheritance.dart
@@ -1,15 +1,11 @@
 void main() {
   
-  Dog dog1 = Dog();
-  dog1.name = 'Rex';
-  dog1.age = 3;
+  Dog dog1 = Dog("Rex", 3);
   dog1.eat();
   dog1.sleep();
   dog1.bark();
 
-  Cat cat1 = Cat();
-  cat1.name = 'Xer';
-  cat1.age = 4;
+  Cat cat1 = Cat("Xer", 4);
   cat1.eat();
   cat1.sleep();
   cat1.meow();
@@ -21,6 +17,8 @@ class Animal {
   String? name;
   int? age;
 
+  Animal(this.name, this.age);
+
   void eat() {
     print("ate.");
   }
@@ -31,12 +29,24 @@ class Animal {
 }
 
 class Dog extends Animal {
+
+  Dog(String name, int age) : super(name, age);
+
   void bark() {
     print("barked.");
+  }
+
+  @override // overriding superclass method
+  void sleep() {
+    super.sleep(); // acessing superclass sleep method()
+    print("dog sleeping");
   }
 }
 
 class Cat extends Animal {
+
+  Cat(String name, int age) : super(name, age);
+
   void meow() {
     print("meowed.");
   }
